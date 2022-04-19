ac09c4acf3e35208b15c604d841e5282f0ca89fd
Kooky Gie <kookygie@gmail.com>
2022-04-19 06:15:04 +0300

    Update config.js

--- config.js
+++ config.js
@@ -22,14 +22,14 @@ global.APIKeys = {
 }
 
 //═══════[modification]════════\\
-global.owner = ['916909137213','6283839980451'] //owner number, u can put multiple owner number, within quotations and seperated by comma.
-global.pemilik = ['916909137213'] //another owner number
-global.premium = ['916909137213'] //premium number
+global.owner = ['254112541130','254112541130'] //owner number, u can put multiple owner number, within quotations and seperated by comma.
+global.pemilik = ['254112541130'] //another owner number
+global.premium = ['254112541130'] //premium number
 global.pengguna = 'Xeon' //username
 global.botnma = 'Cheems Bot-MD' //bot name
-global.ownernma = '🦄Dream Guy Xeon' //owner name
+global.ownernma = '🦄Dream Guy 🇰🇪Kooky' //owner name
 global.packname = 'Cheems Bot' //sticker package name
-global.author = 'By Dream Guy Xeon' //sticker author name
+global.author = 'By Dream Guy Kooky🇰🇪' //sticker author name
 global.sessionName = 'session' //session name
 global.prefa = ['#','!','/',''] //prefix
 global.sp = '⭔' //design
